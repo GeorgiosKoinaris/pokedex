8a5f52c48af245ac8e302400687e91619ef4e785
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-06-07 21:56:20 +0200

    latest update

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const colors = {
     fairy: '#D685AD',
 };
 
+//download API from server/////////////////////////
 
 async function initPokemon() {
     for (let i = 1; i <= pokemonNumber; i++) {
@@ -30,10 +31,7 @@ async function initPokemon() {
     }
 }
 
-
 async function loadPokemon(id) {
-    // let pokemonName = document.getElementById('inputField').value.toLowerCase();
-
     let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     let response = await fetch(url);
     currentPokemon = await response.json();
@@ -41,6 +39,8 @@ async function loadPokemon(id) {
     console.log(currentPokemon);
     renderPokemonCards(currentPokemon);
 }
+
+//render all 151 Pokemon//////////////////////////
 
 function renderPokemonCards(pokemon) {
     const name = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
@@ -50,9 +50,10 @@ function renderPokemonCards(pokemon) {
     const type = pokemon['types'][0]['type']['name'].charAt(0).toUpperCase() + pokemon['types'][0]['type']['name'].slice(1);
     const color = colors[type.toLowerCase()];
 
-    let container = document.getElementById('allPokemon');
 
-    container.innerHTML += `<div id="pokeBox${id}" class="pokeBox" onclick="showPokemon(${pokemon})">
+    let container = document.getElementById('allPokemon');
+    /////////////////////////////////////////////////////////showPokemon() check variable!!!//////////////////////////////////////////////////////////////
+    container.innerHTML += `<div id="pokeBox${id}" class="pokeBox" onclick="showPokemonInfo(${pokemon[id]})"> 
         <h5>${name}</h5>
         <img src="${image}">
         <div style="background-color: rgba(255, 255, 255, 0.5); border-radius: 5px"><b>${type}</b></div>
@@ -63,24 +64,24 @@ function renderPokemonCards(pokemon) {
 }
 
 
+// //////////////////need to check variable for showPokemonInfo()///////////////////////////////////////////////////////////////////////////////////////
+// // function showPokemonInfo(pokemon) {
+// //     const name = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
+// //     const image = pokemon['sprites']['other']['dream_world']['front_default'];
+// //     const number = '#' + pokemon['id'].toString().padStart(3, '0');
+// //     const id = pokemon['id'].toString();
+// //     const type = pokemon['types'][0]['type']['name'].charAt(0).toUpperCase() + pokemon['types'][0]['type']['name'].slice(1);
+// //     const color = colors[type.toLowerCase()];
 
-// function renderPokemonInfo(pokemon) {
-//     document.getElementById('pokemonName').innerHTML = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1); //Name with first character in big letter//
-//     document.getElementById('pokemonImage').src = pokemon['sprites']['other']['dream_world']['front_default'];
-//     document.getElementById('pokemonNumber').innerHTML = '#' + pokemon.id.toString().padStart(3, '0');
-//     document.getElementById('pokemonType').innerHTML = pokemon['types'][0]['type']['name'].charAt(0).toUpperCase() + pokemon['types'][0]['type']['name'].slice(1); //type with first character in big letter//
-//     document.getElementById('stat1').innerHTML = pokemon['stats'][0]['base_stat'];
-//     document.getElementById('stat2').innerHTML = pokemon['stats'][1]['base_stat'];
-//     document.getElementById('stat3').innerHTML = pokemon['stats'][2]['base_stat'];
-//     document.getElementById('stat4').innerHTML = pokemon['stats'][3]['base_stat'];
-//     document.getElementById('stat5').innerHTML = pokemon['stats'][4]['base_stat'];
-//     document.getElementById('stat6').innerHTML = pokemon['stats'][5]['base_stat'];
+// //     document.getElementById('pokemonInfo').classList.remove('d-none');
+// //     document.getElementById('pokemonName').innerHTML = name;
+// //     document.getElementById('pokemonImage').src = image;
+// //     document.getElementById('pokemonNumber').innerHTML = number;
+// //     document.getElementById('pokemonType').innerHTML = type;
+// //     document.getElementById('stat1').innerHTML = pokemon['stats'][0]['base_stat'];
+// //     document.getElementById('stat2').innerHTML = pokemon['stats'][1]['base_stat'];
+// //     document.getElementById('stat3').innerHTML = pokemon['stats'][2]['base_stat'];
+// //     document.getElementById('stat4').innerHTML = pokemon['stats'][3]['base_stat'];
+// //     document.getElementById('stat5').innerHTML = pokemon['stats'][4]['base_stat'];
+// //     document.getElementById('stat6').innerHTML = pokemon['stats'][5]['base_stat'];
 // }
-
-function smallWindow() {
-    if ($(window).width < 960) {
-        alert('Less than 960');
-    } else {
-        alert('More than 960');
-    }
-}
